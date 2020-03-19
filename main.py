10c338fb1014d6ba06a303faeff4908b7866e588
alenatorgasheva <in-fa@mail.ru>
2020-03-19 15:43:20 +0700

    New example and updated main.

--- main.py
+++ main.py
@@ -25,6 +25,38 @@
 # связи     - значения
 
 
+def open_file():
+    file = 'example.txt'
+    ptr = 0
+    while True:
+        try:
+            with open(file, 'r'):
+                pass
+        except FileNotFoundError:
+            print('Файл {} не найден.'.format(file))
+            file = input('Введите имя файла: ')
+            ptr = 0
+        else:
+            return file
+
+
+def text_to_words(file):
+    all_words = []
+    text = ''
+    with open(file, 'r') as file_in:
+        for string in file_in.readlines():
+            text += string
+        text = text.replace('\n', ' ')
+        word = ''
+        for i in text:
+            if i == ' ':
+                all_words.append(word)
+                word = ''
+            else:
+                word += i
+    return all_words
+
+
 def unique_words(lst_of_words):
     # lst_of_unique_words - списочек уникальных слов
     # lst_of_words - список всех слов
@@ -55,7 +87,18 @@ def final_words(lst_of_words):
     return stop_words
 
 
-def generator(number_of_sentences, start_words, lst_of_words, stop_words):
+def dictionary(lst_of_unique_words, all_words):
+    links_and_connections = {}
+    for word in lst_of_unique_words:
+        following_words = []
+        for i in range(len(all_words) - 1):
+            if word == all_words[i] and all_words[i + 1] not in following_words:
+                following_words.append(all_words[i + 1])
+        links_and_connections[word] = following_words
+    return links_and_connections
+
+
+def generator(number_of_sentences, start_words, links_and_connections, stop_words):
     # number_of_sentences - колво предложений
     # start_words - список старт слов
     # lst_of_words - словарь слов
@@ -65,55 +108,27 @@ def generator(number_of_sentences, start_words, lst_of_words, stop_words):
     for _ in range(number_of_sentences):
         word = start_words[random.randint(0, len(start_words) - 1)]
         counter = 1
-        while word[-1] != '.' and counter != 19:
-            print(word, end=' ')
-            word = lst_of_words[word][random.randint(0, len(lst_of_words[word]) - 1)]
+        print(word, end=' ')
+        while word[-1] != '.' and counter < 19:
+            word = links_and_connections[word][random.randint(0, len(links_and_connections[word]) - 1)]
             counter += 1
-        if counter == 19:
+            print(word, end=' ')
+        if counter == 19 and word[-1] != '.':
             print(stop_words[random.randint(0, len(stop_words) - 1)])
-
-
-def dictionary(lst_of_unique_words, lst_of_words):
-    dict = {}
-    for i in range(len(lst_of_unique_words)):
-        lst = []
-        for j in range(len(lst_of_words)):
-            if i == j:
-                lst.append(lst_of_words[j + 1])
-        dict[lst_of_unique_words[i]] = lst
-
-
-def text_generator():
-    file = input()
-    ptr = 0
-    while ptr == 0:
-        try:
-            with open(file, 'r'):
-                pass
-        except FileNotFoundError:
-            print('Файл {} не найден.'.format(file))
-            file = input('Введите имя файла: ')
-            ptr = 0
         else:
-            ptr = 1
-    number = input('Количество генерируемых предложений: ')
-    lst_of_words = []
-    text = ''
-    with open(file, 'r') as file_in:
-        for string in file_in.readlines():
-            text += string
-        text = text.replace('\n', ' ')
-        word = ''
-        for i in text:
-            if i == ' ':
-                lst_of_words.append(word)
-                word = ''
-            else:
-                word += i
-
-    # text - весь текст из файла, \n удалены и заменены на пробел
-    # lst_of_words - списочек всех слов из текста, знаки препинания сохранены (порезано по пробелам)
-    print(text, lst_of_words, sep='\n')
+            print()
 
 
-text_generator()
+def main():
+    file = open_file()
+
+    all_words = text_to_words(file)
+    start_words = initial_words(all_words)
+    stop_words = final_words(all_words)
+    links_and_connections = dictionary(unique_words(all_words), all_words)
+
+    number_of_sentences = int(input('Введите число предложений: '))
+    generator(number_of_sentences, start_words, links_and_connections, stop_words)
+
+
+main()
